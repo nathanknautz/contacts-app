a9534fe450ab433b9d18cfdd997d6bcd716f5f0f
nathanknautz <nate.knautz@gmail.com>
2018-01-16 18:12:11 -0600

    updated with json method

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,27 +1,14 @@
 class ContactsController < ApplicationController
   def one_contact_method
     contact = Contact.first
-    render json: {first_name: contact.first_name,
-                  last_name: contact.last_name,
-                  email: contact.email,
-                  phone_number: contact.phone_number
-    }
+    render json: contact.as_json
       
   end
 
   def all_contacts
     contacts = Contact.all
-    display_contacts = []
 
-    contacts.each do |contact|
-      contact = {first_name: contact.first_name,
-                  last_name: contact.last_name,
-                  email: contact.email,
-                  phone_number: contact.phone_number} 
-      display_contacts << contact
-    end
-
-    render json: display_contacts
+    render json: contacts.as_json
     
   end
 end
