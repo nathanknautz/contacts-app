7a25f68bf0e8afb538fd9b7f3d870491dd988459
nathanknautz <nate.knautz@gmail.com>
2018-01-20 20:37:30 -0600

    updated with restful routes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,14 +1,39 @@
 class ContactsController < ApplicationController
-  def one_contact_method
-    contact = Contact.first
-    render json: contact.as_json
-      
-  end
+ def index
+   contacts = Contact.all 
+   render json: contacts.as_json
+ end
 
-  def all_contacts
-    contacts = Contact.all
+ def create
+   contact = Contact.new(first_name: params[:first_name],
+                        last_name: params[:last_name],
+                        email: params[:email],
+                        phone_number: params[:phone_number]
+                         )
+   contact.save
+   render json: contact.as_json
+ end
 
-    render json: contacts.as_json
-    
-  end
+ def show
+  contact = Contact.find(params[:id])
+  render json: contact.as_json
+ end
+
+ def update
+   contact = Contact.find(params[:id])
+   contact.first_name = params[:first_name] || contact.first_name
+   contact.last_name = params[:last_name] || contact.last_name
+   contact.email = params[:email] || contact.email
+   contact.phone_number = params[:phone_number] || contact.phone_number
+   contact.save 
+   render json: contact.as_json
+ end
+
+ def destroy
+  contact = Contact.find(params[:id])
+  contact.destroy 
+  render json: {message: "successfully deleted contact##{params[:id]}"}
+ end
+
+
 end
