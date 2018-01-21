f0820fb5204b7f88157bb9f066d25d958d60fa8b
nathanknautz <nate.knautz@gmail.com>
2018-01-21 15:07:27 -0600

    updated with model methods

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,27 @@
 class Contact < ApplicationRecord
+
+def friendly_updated_at
+  updated_at.strftime("%b %d, %Y%l:%M %p")
+end
+
+def full_name
+  "#{first_name} #{last_name}"
+end
+
+def japan_number
+  "+81 #{phone_number}" 
+end
+
+def as_json
+  {
+   id: id,
+   first_name: first_name,
+   last_name: last_name,
+   full_name: full_name,
+   email: email,
+   phone_number: japan_number,
+   updated_at: friendly_updated_at
+  }
+end
+
 end
